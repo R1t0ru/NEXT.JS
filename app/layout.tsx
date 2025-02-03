c782218dd2a4a1f433868d89494c10b372ca4419
maximestajszczyk <maxime.stajszczyk@ecole-isitech.fr>
2025-02-03 15:26:04 +0100

    Chapter 3 to 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 
 import "@/app/ui/global.css";
+import { inter } from '@/app/ui/fonts';
+
+//Création du composant RootLayout qui prend en parametre children
+//On destructure pour déclarer children
+//Tous les composants React possèdent des props (props)
+//Exemples :
+//const props = {children : "", autreProps : 0}
+//const children = props.children
+//const autreProps = props.autreProps
 
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
